d44709400b5391c04dcdc7d0e85df220f3615c64
Emre GUCLU <e.guclu@centre-inffo.fr>
2025-03-26 18:26:46 +0100

    ajout consigne readme

--- database/init.sql
+++ database/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS annonce (
 
 INSERT INTO annonce (title, description, adress, mail, date) 
 VALUES 
-    ('Annonce 91', 'Description de lannonce 1', '123 Rue Exemple, Paris', 'contact1@example.com', '2025-03-19 10:00:00'),
+    ('Annonce 1', 'Description de lannonce 1', '123 Rue Exemple, Paris', 'contact1@example.com', '2025-03-19 10:00:00'),
     ('Annonce 2', 'Description de lannonce 2', '456 Avenue Test, Lyon', 'contact2@example.com', '2025-03-20 12:30:00'),
     ('Annonce 3', 'Description de lannonce 3', '789 Boulevard Exemple, Marseille', 'contact3@example.com', '2025-03-21 09:45:00'),
     ('Annonce 4', 'Description de lannonce 4', '101 Rue Alpha, Lille', 'contact4@example.com', '2025-03-22 15:00:00'),
